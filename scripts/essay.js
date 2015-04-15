b09894a6a2953af2dc879218538e4cf4f0cb2038
rodmachen <rodmachen@gmail.com>
2015-04-15 14:53:33 -0500

    clear text after event

--- scripts/essay.js
+++ scripts/essay.js
@@ -2,10 +2,12 @@ var $body = $('body');
 var $results = $('.results') 
 
 $('textarea').on("input", function() {
+  $results.empty();
   var $alltext = $("#main_text").val();
   var words = function (text) {
     return text.split(" ").length;
   };
   var $words = words($alltext);
+  $results.append('<p>How many words?</p>');
   $results.append($words);
 });
