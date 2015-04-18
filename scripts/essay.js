86e3060bc0c5b35340a5ed34a7c49bac232b7fe1
rodmachen <rodmachen@gmail.com>
2015-04-18 14:14:58 -0500

    add comments

--- scripts/essay.js
+++ scripts/essay.js
@@ -1,14 +1,17 @@
-// cache $results to avoid memory overages
+// cache $results to avoid memory problems
 var $results = $('.results');
-// 
+
 // all functionality resides in essayChecker
 function essayChecker() {
   // clear results after any change
+  // also removes initial instructions
   $results.empty();
 
   // variable for the submitted essay
+  // trimmed of extra white space and line breaks
   var $alltext = $("#main_text").val().trim().replace(/(\r\n|\n|\r)/gm," ");
 
+  // wordCount returns the number of words in the essay
   function wordCount() {
     return $alltext.replace(/[^\w\s]|_/g, "")
                    .replace(/\s+/g, " ")
@@ -16,25 +19,30 @@ function essayChecker() {
                    .filter(function(value) {return value !== ""})
                    .length;
   }
-  
+
+  // display he Word count
   var $words = wordCount();
   if ($words > 0) {
     $results.append('<h3>Word count: ' + $words + '</h3>');
     $results.append('<p>Make sure this meets the minimum number of words assigned for this essay.</p>');
   }
 
+  // generic character checking function
   function checkChar(char) {
     return $alltext.split("")
                    .filter(function(value) {return value === char})
                    .length;
   }
 
+  // display number of semicolons
   var $semis = checkChar(";");
   if ($semis > 0) {
     $results.append('<h3>Semicolons: ' + $semis + '</h3>');
     $results.append('<p>Semicolons are difficult to use correctly. Make sure each semicolon is used properly and not just a substitute for ", and".</p>');
   }
 
+  // generic word checking function
+  // ignores punctuation
   function checkTerms(term) {
     return $alltext.replace(/[^\w\s]|_/g, "")
                    .replace(/\s+/g, " ")
@@ -42,13 +50,14 @@ function essayChecker() {
                    .filter(function(value) {return value === term})
                    .length;
   }
-
+  // display uses of the word essay
   var $essay = checkTerms("essay") + checkTerms("Essay")
   if ($essay > 0) {
     $results.append('<h3>Uses of the word “essay”: ' + $essay + '</h3>');
     $results.append('<p>Please do not use the word “essay” when writing an essay, not even in the title.</p>');
   }
 
+  // display uses of 1st and 2nd person terms
   var $I = checkTerms("I");
   var $me = checkTerms("me");
   var $we = checkTerms("we") + checkTerms("We");
@@ -59,6 +68,7 @@ function essayChecker() {
     $results.append('<p>Please do write in 1st or 2nd person. Use 3rd person instead.</p>');
   }
 
+  // a hopeful farewell
   $results.append('<h2>Good luck!</h2>');
 }
 
