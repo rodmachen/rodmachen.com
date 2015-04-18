4b22c871730e8cc7186a5df86a130c44cbc6f51b
rodmachen <rodmachen@gmail.com>
2015-04-18 13:48:36 -0500

    refactor and rename functions

--- scripts/essay.js
+++ scripts/essay.js
@@ -8,47 +8,33 @@ function essayChecker() {
 
   // variable for the submitted essay
   var $alltext = $("#main_text").val().trim().replace(/(\r\n|\n|\r)/gm," ");
-  console.log($alltext.split(" "))
-  var makeWordList = function (text) {
-    return text.split(" ");
+
+  function wordCount() {
+    return $alltext.replace(/[^\w\s]|_/g, "")
+                   .replace(/\s+/g, " ")
+                   .split(" ")
+                   .filter(function(value) {return value !== ""})
+                   .length;
   }
-  var makeCharList = function (text) {
-    return text.split("");
-  }
-  var words = function (func, text) {
-    return func(text).filter(function(value) {return value !== ""})
-                     .length;
-  };
-  var $words = words(makeWordList, $alltext);
+  
+  var $words = wordCount();
   if ($words > 0) {
-    $results.append('<h3>Number of words: ' + $words + '</h3>');
+    $results.append('<h3>Word count: ' + $words + '</h3>');
     $results.append('<p>Make sure this meets the minimum number of words assigned for this essay.</p>');
   }
 
-  var semi = function (text) {
-    var filtered = makeCharList(text).filter(function(value) {
-      return value === ";"
-    });
-    return filtered.length;
-  };
-
-  var $semi = semi($alltext);
-  if ($semi > 0) {
-    $results.append('<h3>Number of semicolons: ' + $semi + '</h3>');
-    $results.append('<p>Semicolons are difficult to use correctly. Make sure each semicolon is properly used and not just a substitute for ", and".</p>');
+  function checkChar(char) {
+    return $alltext.split("")
+                   .filter(function(value) {return value === char})
+                   .length;
   }
 
-  var essay = function (text) {
-    var list = text.split(" ");
-    var filtered = list.filter(function(value) {
-      if (value.slice(0, 5) === "essay" || 
-          value.slice(0, 5) === "Essay") {
-      return value;
-      }
-    });
-    return filtered.length;
-  };
-  // var $essay = essay($alltext);
+  var $semis = checkChar(";");
+  if ($semis > 0) {
+    $results.append('<h3>Semicolons: ' + $semis + '</h3>');
+    $results.append('<p>Semicolons are difficult to use correctly. Make sure each semicolon is used properly and not just a substitute for ", and".</p>');
+  }
+
   function checkTerms(term) {
     return $alltext.replace(/[^\w\s]|_/g, "")
                    .replace(/\s+/g, " ")
@@ -56,11 +42,13 @@ function essayChecker() {
                    .filter(function(value) {return value === term})
                    .length;
   }
+
   var $essay = checkTerms("essay") + checkTerms("Essay")
   if ($essay > 0) {
     $results.append('<h3>Uses of the word “essay”: ' + $essay + '</h3>');
     $results.append('<p>Please do not use the word “essay” when writing an essay, not even in the title.</p>');
   }
+
   var $I = checkTerms("I");
   var $me = checkTerms("me");
   var $we = checkTerms("we") + checkTerms("We");
@@ -70,14 +58,11 @@ function essayChecker() {
     $results.append('<h3>Uses of “I”: ' + $I + ', “me”: ' + $me + ', “we”: ' + $we + ', “you”: ' + $you + '</h3>');
     $results.append('<p>Please do write in 1st or 2nd person. Use 3rd person instead.</p>');
   }
+
   $results.append('<h2>Good luck!</h2>');
 }
 
 // watch for input and display
 $('textarea').on("input", essayChecker);
-
-function wordSearch(fullText, elements) {
-  
-}
 
 
